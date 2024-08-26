f0a2828026667641f74b5c616b64f7114e5af0d9
Ricardoibanez <ibanezricardoalejandro@gmail.com>
2024-08-26 16:47:14 +0200

    Update Ejercicio2.py

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -2,13 +2,7 @@
 Crea un programa que calcule el monto total a pagar en un restaurante, incluyendo
 una propina del 15% sobre el total de la cuenta.'''
 
-camarones = 10
-alcachofas = 10
-cocacola = 2
+cuenta = float(input('Ingrese la totalidad de la cuenta sin propina: '))
+total_cuenta = cuenta * 1.15
+print(f'El monto total a pagar con un 15 % de propina es: ', total_cuenta, '€')
 
-cuenta = [camarones, alcachofas, cocacola]
-euros = 0
-for plato in cuenta:
-  euros += plato
-total = euros * 1.15
-print(total)
